f123ec2d4826c60fa5315ea36e3c74d18b3c4058
Ignacio Serrano Rodriguez <ignacioserranorodriguez@gmail.com>
2023-02-27 09:58:40 +0100

    modificacion de nombres y generacion del .class

--- hola.java
+++
@@ -1,9 +0,0 @@
-public class hola
-{
-	public static void main(String[] args)
-	{
-		System.out.println("hola");
-		System.out.println("Segunda linea");
-		System.out.println("Linea del segundo repositorio");
-	}
-}
